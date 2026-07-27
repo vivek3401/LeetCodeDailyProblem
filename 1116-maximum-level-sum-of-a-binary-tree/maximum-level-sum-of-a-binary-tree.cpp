984ba4a0242ec6392254eb76a9cabfeab9ee54d7
vivek3401 <vivek875474@gmail.com>
2026-07-27 18:16:19 +0530

    Time: 7 ms (28.14%) | Memory: 109.4 MB (60.43%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -12,32 +12,26 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        map<int,vector<int>> mp;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,1});
+        queue<TreeNode*> q;
+        q.push(root);
+        int level=1;
+        int count=1;
+        int ans=INT_MIN;
         while(!q.empty()){
             int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* curr=q.front().first;
-                int dis=q.front().second;
-                q.pop();
-                mp[dis].push_back(curr->val);
-                if(curr->left) q.push({curr->left,dis+1});
-                if(curr->right) q.push({curr->right,dis+1});
-            }
-        }
-        int ans=INT_MIN;
-        int level=0;
-        for(auto it : mp){
-            int size=it.second.size();
             int sum=0;
             for(int i=0;i<size;i++){
-                sum+=it.second[i];
+                TreeNode*curr=q.front();
+                q.pop();
+                sum+=curr->val;
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
             }
             if(sum>ans){
-                level=it.first;
+                level=count;
                 ans=sum;
             }
+            count++;
         }
         return level;
     }
